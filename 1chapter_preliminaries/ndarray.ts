f633ab7caa477dd648c074dc4e0ed466f6090348
TLightSky <wangchao719@gmail.com>
2022-07-08 20:04:43 +0800

    add more ndarray

--- 1chapter_preliminaries/ndarray.ts
+++ 1chapter_preliminaries/ndarray.ts
@@ -1,5 +1,11 @@
 import * as tf from '@tensorflow/tfjs-node-gpu';
 import {Tensor1D} from "@tensorflow/tfjs";
 
-let x: Tensor1D = tf.range(12, 1)
-console.log(x)
+let x: Tensor1D = tf.range(0, 12)
+x.print(true)
+console.log(x.shape)
+// console.log(tf.size(x))
+
+let X: Tensor1D = x.reshape([3, 4])
+X.print(true)
+
