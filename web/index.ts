2690918519b298d2510a487e462de3ddeb3d79c3
TLightSky <wangchao719@gmail.com>
2022-07-10 21:40:28 +0800

    finish ndarray

--- web/index.ts
+++ web/index.ts
@@ -1,5 +1,5 @@
 import * as tf from '@tensorflow/tfjs';
-import {Tensor1D} from "@tensorflow/tfjs";
+import {Tensor1D, Variable} from "@tensorflow/tfjs";
 
 (function () {
     let x: Tensor1D = tf.range(0, 12)
@@ -51,10 +51,36 @@ import {Tensor1D} from "@tensorflow/tfjs";
     outText += `${tf.range(0, 12, 1, 'float32').reshape([3, 4])}\n`
     outText += `\nlet Y: Tensor1D = tf.tensor([[2.0, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]]):\n`
     outText += `${tf.tensor([[2.0, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])}\n`
-    // outText += `\ntf.range(0, 12, 1, 'float32').reshape([3, 4]):\n`
-    // outText += `${tf.range(0, 12, 1, 'float32').reshape([3, 4])}\n`
+    outText += `\ntf.concat([X, Y], 0), tf.concat([X, Y], 1):\n`
+    outText += `${tf.concat([X, Y], 0)}, \n${tf.concat([X, Y], 1)}\n`
+    outText += `\ntf.equal(X, Y):\n`
+    outText += `${tf.equal(X, Y)}\n`
 
-    tf.concat([X, Y], 0), tf.concat([X, Y], 1)
+    outText += `\nX.sum():\n`
+    outText += `${X.sum()}\n`
+
+    let a: Tensor1D = tf.range(0, 3).reshape([3, 1])
+    let b: Tensor1D = tf.range(0, 2).reshape([1, 2])
+    outText += `\nlet a: Tensor1D = tf.range(0, 3).reshape([3, 1]):\n`
+    outText += `${a}\n`
+    outText += `\nlet b: Tensor1D = tf.range(0, 2).reshape([1, 2]):\n`
+    outText += `${b}\n`
+
+    outText += `\na.add(b):\n`
+    outText += `${a.add(b)}\n`
+
+    // outText += `${X}\n`
+    outText += `\nX.slice(1, 3):\n`
+    outText += `${'???'}\n`
+
+    let XVar: Variable = tf.variable(X)
+    outText += `\nlet XVar: Variable = tf.variable(X):\n`
+    outText += `${XVar}\n`
+
+    outText += `\nX.slice(1, 3).assign(9):\n`
+    outText += `${'???'}\n`
+
+    outText += `@tf.function? ${'???'}\n`
 
     let outEle = document.getElementById('ndarray')
     if (outEle !== null) {
