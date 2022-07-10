c5a06ffd902dc3436f9660882f7e29aea67c7c6c
TLightSky <wangchao719@gmail.com>
2022-07-10 22:54:44 +0800

    finish linear-algebra

--- web/index.ts
+++ web/index.ts
@@ -1,10 +1,10 @@
 import * as tf from '@tensorflow/tfjs';
-import {Tensor1D, Variable} from "@tensorflow/tfjs";
+import {Tensor1D, Tensor2D, Tensor3D, Variable} from "@tensorflow/tfjs";
 
 (function () {
     let x: Tensor1D = tf.range(0, 12)
 
-    let outText: string = '';
+    let outText: string = ''
     outText += `\nrange(0,12):\n`
     outText += `${x}\n`
     outText += `\nshape:\n`
@@ -87,3 +87,99 @@ import {Tensor1D, Variable} from "@tensorflow/tfjs";
         outEle.innerText = outText
     }
 })();
+
+(async function () {
+    let x: Tensor1D = tf.range(0, 4)
+    let outText: string = ''
+
+    outText += `\nlet x: Tensor1D = tf.range(0, 4):\n`
+    outText += `${x}\n`
+
+    outText += `\n(await x.data())[3]):\n`
+    outText += `${(await x.data())[3]}\n`
+
+    outText += `\nx.size:\n`
+    outText += `${x.size}\n`
+
+    outText += `\nx.shape:\n`
+    outText += `${x.shape}\n`
+
+    let A: Tensor2D = tf.range(0, 20).reshape([5, 4])
+    outText += `\nlet A: Tensor2D= tf.range(0, 20).reshape([5, 4]):\n`
+    outText += `${A}\n`
+    outText += `\nA.transpose():\n`
+    outText += `${A.transpose()}\n`
+
+    let B: Tensor2D = tf.tensor2d([[1, 2, 3], [2, 0, 4], [3, 4, 5]])
+    outText += `\nlet B: Tensor2D = tf.tensor2d([[1, 2, 3], [2, 0, 4], [3, 4, 5]]):\n`
+    outText += `${B}\n`
+    outText += `\nB.equal(B.transpose()):\n`
+    outText += `${B.equal(B.transpose())}\n`
+
+    let X: Tensor3D = tf.range(0, 24).reshape([2, 3, 4])
+    outText += `\nlet X: Tensor3D = tf.range(0, 24).reshape([2, 3, 4]):\n`
+    outText += `${X}\n`
+
+    outText += `\nA:\n`
+    outText += `${A}\n`
+    outText += `\nA.add(A):\n`
+    outText += `${A.add(A)}\n`
+    outText += `\nA.mul(A):\n`
+    outText += `${A.mul(A)}\n`
+
+    outText += `\nX.add(2):\n`
+    outText += `${X.add(2)}\n`
+    outText += `\nX.mul(2):\n`
+    outText += `${X.mul(2)}\n`
+
+    outText += `\nA.sum():\n`
+    outText += `${A.sum()}\n`
+    outText += `\nA.sum(0):\n`
+    outText += `${A.sum(0)}\n`
+    outText += `\nA.sum(1):\n`
+    outText += `${A.sum(1)}\n`
+    outText += `\nA.sum([0, 1]):\n`
+    outText += `${A.sum([0, 1])}\n`
+    outText += `\nA.mean(0):\n`
+    outText += `${A.mean(0)}\n`
+    outText += `\nA.sum(1, true):\n`
+    outText += `${A.sum(1, true)}\n`
+    outText += `\nA.div(A.sum(1, true)):\n`
+    outText += `${A.div(A.sum(1, true))}\n`
+
+    outText += `\nA.cumsum(0):\n`
+    outText += `${A.cumsum(0)}\n`
+
+    let y: Tensor1D = tf.ones([4], 'float32')
+    outText += `\nlet y = tf.ones([4], 'float32'):\n`
+    outText += `${y}\n`
+    outText += `\nx.dot(y):\n`
+    outText += `${x.dot(y)}\n`
+
+    outText += `\nx.mul(y).sum():\n`
+    outText += `${x.mul(y).sum()}\n`
+
+    outText += `\ntf.linalg.matvec(A, x):\n`
+    outText += `${'???'}\n`
+
+    let B2 = tf.ones([4, 3], 'float32')
+    outText += `\nA.matMul(B2):\n`
+    outText += `${A.matMul(B2)}\n`
+
+    let u: Tensor1D = tf.tensor1d([3.0, -4.0])
+    outText += `\nlet u: Tensor1D = tf.tensor1d([3.0, -4.0]):\n`
+    outText += `${u}\n`
+    outText += `\nu.norm():\n`
+    outText += `${u.norm()}\n`
+    outText += `\ntf.ones([4, 9]).norm():\n`
+    outText += `${tf.ones([4, 9]).norm()}\n`
+
+    outText += `\nX.size:\n`
+    outText += `${X.size}\n`
+
+
+    let outEle = document.getElementById('linear-algebra')
+    if (outEle !== null) {
+        outEle.innerText = outText
+    }
+})()
